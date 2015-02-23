60580d6895e18a7f439df1e8e623a48e335b4cdc
mykia <mykiajls@gmail.com>
2015-02-23 09:41:01 -0600

    Adds greeting and time to homepage

--- splash_home/app/controllers/home_controller.rb
+++ splash_home/app/controllers/home_controller.rb
@@ -1,4 +1,20 @@
 class HomeController < ApplicationController
   def index
-  end
+  	right_now = Time.now
+  	@current_time = right_now.to_formatted_s(:long) #use the stamp gem to make this look better
+  	
+
+  	# Adjust message by time of day
+  	@greet_message = case right_now.hour
+  	when 5..11
+  		"Good Morning, Sugar!"
+  	when 12..17
+  		"Good Afternoon, HoneyBun!"
+  	when 18..20
+  		"Good Night, Dahlin!"
+  	when 21..23, 0..4
+  		"You should be sleep, Sweetheart!"
+  	end
+
+ end
 end
